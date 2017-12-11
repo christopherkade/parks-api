92856284e27a59da71ecbb50f9c4f72becd06c61
christopher <christopher.kade@epitech.eu>
2017-12-11 15:51:22 +0000

    Updated header values Other tweaks

--- server.js
+++ server.js
@@ -8,25 +8,15 @@ const express = require('express'),
 app.use(bodyParser.json());
 app.use(function(req, res, next) {
   res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT');
-  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+  res.setHeader('Access-Control-Allow-Methods', 'GET');
+  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With, content-type');
   res.setHeader('Access-Control-Allow-Credentials', true);
   next();
 });
 
 // API ROUTES
 app.get('/', (req, res) => {
-  res.send('Parks API works !');
-});
-
-// GET all park names
-app.get('/api/parks', (req, res) => {
-  let parks = [];
-  for (var park in Themeparks.Parks) {
-    console.log("* " + new Themeparks.Parks[park]().Name + " (" + park + ")");
-    parks.push(new Themeparks.Parks[park]().Name)
-  }
-  res.status(200).json(parks);
+  res.send('Parks API');
 });
 
 // DISNEYLAND PARIS
